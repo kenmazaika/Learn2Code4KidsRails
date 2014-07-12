9439b85e3b276bba53b19f4cca54399dfae1260d
Ken Mazaika <kmazaika@gmail.com>
2014-07-12 16:39:46 -0400

    Start dumping stuff on the syllabus page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def closest_upcoming_monday
+	today = Time.now.strftime("%A")
+	if today == "Monday"
+		return Time.now
+	else
+		Chronic.parse('next monday', :ambiguous_time_range => :none)
+	end
+  end
+
+
+  def closest_weekday_in_future
+  	tomorrow = Time.now + 1 .day
+  	return closest_upcoming_monday if ["Saturday", "Sunday"].include?(tomorrow.strftime("%A"))
+  	tomorrow
+  end
+
 end
